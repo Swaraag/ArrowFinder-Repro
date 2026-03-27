4fae55c2f927e4d0cbe125a8c76dfb021f069037
Swaraag <swaraag.sistla@gmail.com>
2026-03-27 16:45:32 -0700

    Added architectures and graph_transformer.py
    Created an architectures folder within reaction_prediction/atom which contains a graph_transformer.py file which will soon implement a custom GPS Graph Transformer.

---
+++ reaction_prediction/atom/architectures/graph_transformer.py
@@ -0,0 +1,5 @@
+# Build a custom General, Powerful, Scalable (GPS) Graph Transformer using Pytorch Geometric 
+# Raw CSV data is used to build graphs per epoch through SMILES representations, and a custom class
+# is used to integrate the architecture in the existing generate_predictions.py inference pipeline.
+
+
