0dcd464ee6c817e50abba396d25e64f3615858c1
Swaraag <swaraag.sistla@gmail.com>
2026-03-27 20:09:15 -0700

    Bug fixes upon testing

--- reaction_prediction/atom/architectures/modules/mol_graph_utils.py
+++ reaction_prediction/atom/architectures/modules/mol_graph_utils.py
@@ -21,11 +21,13 @@ class CSVToGraphs:
         """
         reaction, arrows, source_atom, sink_atom = row
         if self._atom_type_str == 'source':
-            s_atom_ = source_atom
+            s_atom = source_atom
         elif self._atom_type_str == 'sink':
             s_atom = sink_atom
 
         reactants = reaction.split(">>")[0]
+
+        data_objs = []
 
         for mol_smi in reactants.split("."):
             try:
@@ -36,16 +38,22 @@ class CSVToGraphs:
                 mol = mol_with_hydrogens(smi)
                 x = mol.GetAtoms()
                 
-                edge_index = self.create_edge_index(self, mol)
+                edge_index = self.create_edge_index(mol)
+                # skip molecules entirely if they have no edges, bc not much for GT to reason about
+                if edge_index.shape[1] == 0:
+                    continue
 
                 edge_attr = self.create_edge_attr(edge_index, mol)
 
-                y = self.create_y(x)
+                y = self.create_y(x, s_atom)
                 
-                data = Data(x=x, y=y, edge_index=edge_index, edge_attr=edge_attr)
+                data = Data(x=x, y=y, edge_index=edge_index, edge_attr=edge_attr, num_nodes=mol.GetNumAtoms())
+                data.validate(raise_on_error=True)
+
                 transform = AddRandomWalkPE(walk_length=20, attr_name="random_walk")
-                return transform(data)
+                data_objs.append(transform(data))
                 # positional encoding data stored in data.random_walk
+        return data_objs
 
     def process_csv(self, csv_path):
         # this list is gonna contain both atom information (torch_geometric.data.Data.x) 
@@ -58,12 +66,12 @@ class CSVToGraphs:
                 all_data_objs.extend(self.reaction_to_graph_data(row))
         return all_data_objs
     
-    def create_y(self, x):
+    def create_y(self, x, s_atom):
         y = torch.zeros((len(x)))
 
         # s_atom stores either all sources, or all sink
         s_atom = mol_with_hydrogens(s_atom)
-        for special_atom in s_atom:
+        for special_atom in s_atom.GetAtoms():
             for mol_atom_idx, mol_atom in enumerate(x):
                 if mol_atom.GetIdx() == special_atom.GetIdx():
                     y[mol_atom_idx] = 1
@@ -86,16 +94,16 @@ class CSVToGraphs:
         edge_attr = torch.zeros((len(edge_index), len(edge_attr_names)))
         
         torch.zeros((len(edge_index), len(edge_attr_names)))
-        for edge in len(edge_index):
+        for edge in range(len(edge_index)):
             for attr_index, attr_func in enumerate(edge_attr_names):
                 # using the func tuples to add edge attrs to the edges
-                method = getattr(mol.GetBondBetweenAtoms(edge_index[0][edge], edge_index[1][edge]), attr_func)
+                method = getattr(mol.GetBondBetweenAtoms(edge_index[0][edge].item(), edge_index[1][edge].item()), attr_func)
                 edge_attr[edge][attr_index] = method()
         
         return edge_attr
 
 if __name__ == "__main__":
-    mol_to_graph = CSVToGraphs()
+    mol_to_graph = CSVToGraphs("source")
 
     csv_path = "data/mc_train_fold0/reformatted/train.txt"
     print(mol_to_graph.process_csv(csv_path))
